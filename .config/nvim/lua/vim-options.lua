846cd87874dc3ed622d0e619051440e061daa53c
Karl Kowal <kowal.karl99@gmail.com>
2024-06-14 12:14:35 +0200

    nvim

--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -1,12 +1,19 @@
+
 --Keybinds
 vim.keymap.set("v", "<Tab>",  ">gv", {})
 vim.keymap.set("v", "<S-Tab>",  "<gv", {})
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+vim.keymap.set("n", "_d",vim.diagnostic.goto_next, {})
+vim.keymap.set("n", "_D",vim.diagnostic.goto_prev, {})
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, {})
+vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
 
 --Options
 vim.g.mapleader = ' '
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-vim.bo.softtabstop = 2
+vim.bo.softtabstop = 4
+vim.opt.hlsearch = true
