85b4209f5318b91f566f20b466c2b69941d9a56e
K0nny16 <kowal.karl99@gmail.com>
2024-06-22 19:39:14 +0200

    fixade scriptet

--- .config/setup_symlinks.sh
+++ .config/setup_symlinks.sh
@@ -2,23 +2,22 @@
 
 #Removing premade folders.
 rm -rf ~/.config/i3
-rm -rf ~/.config/neofetch
 
 #Setting up symlinks
-ln -s ~/dotfiles/.config/alacritty ~/.config/alacritty
-ln -s ~/dotfiles/.zshrc ~/zshrc
-ln -s ~/dotfiles/.config/i3 ~/.config/i3
-ln -s ~/dotfiles/.config/polybar ~/.config/polybar
-ln -s ~/dotfiles/.config/starship.toml ~/.config/starship.toml
-ln -s ~/dotfiles/.config/picom.conf ~/.config/picom.conf
-ln -s ~/dotifles/.config/neofetch ~/.config/neofetch
-ln -s ~/dotfiles/.config/nvim ~/.config/nvim
+ln -s ~/dotfiles/.config/alacritty /home/kowal/.config/alacritty
+ln -s ~/dotfiles/.zshrc /home/kowal/.zshrc
+ln -s ~/dotfiles/.config/i3 /home/kowal/.config/i3
+ln -s ~/dotfiles/.config/polybar /home/kowal/.config/polybar
+ln -s ~/dotfiles/.config/starship.toml /home/kowal/.config/starship.toml
+ln -s ~/dotfiles/.config/picom.conf /home/kowal/.config/picom.conf
+ln -s ~/dotifles/.config/neofetch /home/kowal/.config/neofetch
+ln -s ~/dotfiles/.config/nvim /home/kowal/.config/nvim
 
 echo "Dotfiles symlinked successfully!"
 
 #Cloning shell tools
 mkdir -p ~/.config/.zshplugins
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.config/.zshplugins
-git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.config/.zshplugins
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /home/kowal/.config/.zshplugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions /home/kowal/.config/.zshplugins/zsh-autosuggestions
 
 echo "Shell pluggins added successfully!"
