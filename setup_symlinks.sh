64dbf27801b8d37335f9a3679438db82110d16f2
Karl Kowal <147043479+K0nny16@users.noreply.github.com>
2024-06-23 11:15:18 +0200

    Update setup_symlinks.sh
    neofetch fix

--- setup_symlinks.sh
+++ setup_symlinks.sh
@@ -12,7 +12,7 @@ ln -s ~/dotfiles/.config/i3 /home/$USER/.config/i3
 ln -s ~/dotfiles/.config/polybar /home/$USER/.config/polybar
 ln -s ~/dotfiles/.config/starship.toml /home/$USER/.config/starship.toml
 ln -s ~/dotfiles/.config/picom.conf /home/$USER/.config/picom.conf
-ln -s ~/dotifles/.config/neofetch/config.conf /home/$USER/.config/neofetch
+ln -s ~/dotifles/.config/neofetch/config.conf /home/$USER/.config/neofetch/config.conf
 ln -s ~/dotfiles/.config/nvim /home/$USER/.config/nvim
 
 echo "Dotfiles symlinked successfully!"
